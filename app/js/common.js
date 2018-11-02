07b6d64bc508be1dfe576cedfc1d9981985a8b60
AlinaCherkasova <alina.ch1202@gmail.com>
2018-11-02 13:18:18 +0200

    clock and date function v 1.0

--- app/js/common.js
+++ app/js/common.js
@@ -1,32 +1,55 @@
+//toggle
 function toggleSidebar(){
 	document.getElementById('toggleSidebar').classList.toggle('active');
     document.getElementById('content').classList.toggle('active');
 	document.getElementById('toggleSpan').classList.toggle('toggle');
 };
 
+//clock
 function getTime(){
+	const HOURS = document.querySelector('#hours');
+    const MINUTES = document.querySelector('#minutes');
+    const SECONDS = document.querySelector('#seconds');
+
 	var date = new Date();
-	var year = date.getFullYear();
-	var month = date.getMonth();
-	var day = date.getDate();
-	var hours = date.getHours();
-	var minutes = date.getMinutes();
-	var seconds = date.getSeconds();
+	var getHours = date.getHours();
+	var getMinutes = date.getMinutes();
+	var getSeconds = date.getSeconds();
 
-	function dateTime(day) {
-		return (day < 10)
-		? '0' + day
-		: day;
+	function time(num) {
+		return (num < 10)
+		? '0' + num
+		: num;
 	}
 
-	function time(seconds) {
-		return (seconds < 10)
-		? '0' + seconds
-		: seconds;
-	}
-
-	document.getElementById('date').innerHTML = `${dateTime(day)}.${dateTime(month)}.${dateTime(year)}`;
-	document.getElementById('clock').innerHTML = `${time(hours)} : ${time(minutes)} : ${time(seconds)}`;
+    HOURS.innerHTML = time(getHours);
+    MINUTES.innerHTML = time(getMinutes);
+    SECONDS.innerHTML = time(getSeconds);
 	setTimeout(getTime, 1000);
 };
 getTime();
+
+//date
+function getDate(){
+    const DAY = document.querySelector('#day');
+    const MONTH = document.querySelector('#month');
+    const YEAR = document.querySelector('#year');
+
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    var day = date.getDate();
+
+    function dateTime(day) {
+        return (day < 10)
+            ? '0' + day
+            : day;
+    }
+
+    DAY.innerHTML = dateTime(day);
+    MONTH.innerHTML = dateTime(month);
+    YEAR.innerHTML = dateTime(year);
+    // document.getElementById('date').innerHTML = `${dateTime(day)}.${dateTime(month)}.${dateTime(year)}`;
+    setTimeout(getTime, 1000);
+};
+getDate();
